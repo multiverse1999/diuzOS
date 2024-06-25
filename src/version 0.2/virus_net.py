615f68833f6be46e59bd3884647e29df4d878717
multiverse1999 <77222329+multiverse1999@users.noreply.github.com>
2024-06-25 22:36:56 +0900

    Update virus_net.py

--- src/version 0.2/virus_net.py
+++ src/version 0.2/virus_net.py
@@ -1,3 +1,11 @@
+# SPDX-License-Identifier: GPL-3.0 
+'''*
+   * diuz/version 0.2/virus_net.py
+   *
+   * Copyright (C) 2022 multiverse49
+   *
+   * This file is the virus
+   *'''
 import net
 import time
 
