616e31b639166384067a36a21d10497ef90d9791
multiverse1999 <77222329+multiverse1999@users.noreply.github.com>
2024-06-25 22:42:22 +0900

    Update boot.py

--- src/version 0.2/boot.py
+++ src/version 0.2/boot.py
@@ -1,10 +1,10 @@
 # SPDX-License-Identifier: GPL-3.0 
 '''*
-   * diuz/version 0.1/boot.py
+   * diuz/version 0.2/boot.py
    *
-   * Copyright (C) 2022 Zotep 
+   * Copyright (C) 2022 multiverse49
    *
-   * This file is the bootloader for the Diuz kernel
+   * this file is boot
    *'''
 import kernel
 import os.path
