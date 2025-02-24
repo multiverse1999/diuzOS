15acfc16ad0661a50ad1e5878f0e70ddd2b1474d
multiverse1999 <77222329+multiverse1999@users.noreply.github.com>
2025-02-24 11:04:48 +0000

    Update fs.py

--- src/fs.py
+++ src/fs.py
@@ -6,44 +6,46 @@
    *
    * this file is file system
    *'''
-def Disk(operation: str, disk: dict, args: dict):
-    if operation == "FCREATE":
-        disk[args["razdel"]]["file"] = {"1": args["desc"], "2": "FILE"}
-    elif operation == "CONFCREATE":
-        disk["reg"][args["param"]] = {"1": args["desc"], "2": "CONF"}
-    elif operation == "RCREATE":
-        disk[args["razdel"]] = {}
-    elif operation == "FREMOVE":
-        disk[args["razdel"]][args["file"]] = None
-    elif operation == "DRESET":
-        disk = {"C": {}, "reg": {}}
-file_system = {"C": {}, "reg": {}}
+def disk(operation: str, _disk: dict, args: dict):
+    if operation == '''FCREATE''':
+        _disk[args['''directory''']]['''file'''] = {'''1''': args['''desc'''], '''2''': '''FILE'''}
+    elif operation == '''CONFCREATE''':
+        _disk['''reg'''][args['''param''']] = {'''1''': args['''desc'''], '''2''': '''CONF'''}
+    elif operation == '''RCREATE''':
+        _disk[args['''directory''']] = {}
+    elif operation == '''FREMOVE''':
+        _disk[args['''directory''']][args['''file''']] = None
+    elif operation == '''DRESET''':
+        _disk = {'''c''': {}, '''reg''': {}}
+_fs = {'''c''': {}, '''reg''': {}}
 
-def uExpRun():
-    global file_system
-    print("Help: \n file create (partition name)/(file) (content) - create file")
-    print("file remove (partition name)/(file name) - remove file \n partition create (partition name) - create partition")
+def dfs():
+    global _fs
+    print('''help:\n''')
+    print('''file mk (directory name)/(file) (content) - create file\n''')
+    print('''file rm (directory name)/(file name) - remove file\n''')
+    print('''dir mk (directory name) - create directory''')
     while True:
         try:
-            action = input(">> ")
+            action = input('''dfs> ''')
             sa = action.split()
-            if sa[0] == "file":
-                if sa[1] == "create":
-                    ss = sa[2].replace("/", " ")
+            if sa[0] == '''file''':
+                if sa[1] == '''mk''':
+                    ss = sa[2].replace('''/''', ''' ''')
                     ss = ss.split()
-                    Disk("FCREATE", file_system, {"file": ss[1], "razdel": ss[0], "desc": sa[3]})
-                    print(f"File {ss[1]} succesfully created! ")
-                elif sa[1] == "remove":
-                    sa = sa[2].replace("/", " ")
+                    disk('''FCREATE''', _fs, {'''file''': ss[1], '''directory''': ss[0], '''desc''': sa[3]})
+                    print(f'''file {ss[1]} succesfully created!''')
+                elif sa[1] == '''rm''':
+                    sa = sa[2].replace('''/''', ''' ''')
                     sa = sa.split()
-                    Disk("FREMOVE", file_system, {"file": sa[1], "razdel": sa[0]})
-                    print(f"File {sa[1]} succesfully removed! ")
-            elif sa[0] == "partition":
-                if sa[1] == "create":
-                    Disk("RCREATE", file_system, {"razdel": sa[2]})
-                    print("Partition created!")
-            elif sa[0] == "back":
-               print("\n")
+                    disk("FREMOVE", _fs, {'''file''': sa[1], '''directory''': sa[0]})
+                    print(f'''file {sa[1]} succesfully removed!''')
+            elif sa[0] == '''dir''':
+                if sa[1] == '''mk''':
+                    disk('''RCREATE''', _fs, {'''directory''': sa[2]})
+                    print('''directory created!''')
+            elif sa[0] == '''back''':
+               print('''\n''')
                break
         except:
             pass
