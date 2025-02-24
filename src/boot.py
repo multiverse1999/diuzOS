f63c5d83017b65569c49b61eaf0d141b38642e4d
multiverse1999 <77222329+multiverse1999@users.noreply.github.com>
2025-02-24 16:51:20 +0000

    Update boot.py

--- src/boot.py
+++ src/boot.py
@@ -11,89 +11,89 @@ import os.path
 import time
 from progress.bar import IncrementalBar
 
-def text_to_bits(text, encoding = '''utf-8''', errors = '''surrogatepass'''):
-    bits = bin(int.from_bytes(text.encode(encoding, errors), '''big'''))[2:]
+def text_to_bits(text, encoding = "utf-8", errors = "surrogatepass"):
+    bits = bin(int.from_bytes(text.encode(encoding, errors), "big"))[2:]
     return bits.zfill(8 * ((len(bits) + 7) // 8))
   
 def install():
-   nameu = input('''\nyour own name: ''')
-   passu = input('''your own pass: ''')
-   ynn = input('''do you want to install a diuz os? [y/n]: ''')
+   nameu = input("\nyour own name: ")
+   passu = input("your own pass: ")
+   ynn = input("do you want to install a diuz os? [y/n]: ")
    text_to_bits(nameu)
    text_to_bits(passu)
 
-   if ynn == '''y''':
-      with open('''name.nu''', '''w''') as file:
+   if ynn == "y":
+      with open("name.nu", "w") as file:
          file.write(text_to_bits(nameu))
-      with open('''pass.pu''', '''w''') as file:
+      with open("pass.pu", "w") as file:
          file.write(text_to_bits(passu))
 
       bar = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-      binarys = IncrementalBar('''installing libpy''', max = len(bar))
+      binarys = IncrementalBar("installing libraries", max = len(bar))
       for item in bar:
          binarys.next()
          time.sleep(1)
       binarys.finish()
 
       bar = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-      explorer = IncrementalBar('''installing explorer''', max = len(bar))
+      explorer = IncrementalBar("installing explorer", max = len(bar))
       for item in bar:
          explorer.next()
          time.sleep(2)
       explorer.finish()
 
       bar = [1, 2, 3, 4, 5, 6]
-      antivirus = IncrementalBar('''installing commands''', max = len(bar))
+      antivirus = IncrementalBar("installing commands", max = len(bar))
       for item in bar:
          antivirus.next()
          time.sleep(1.5)
       antivirus.finish()
 
       bar = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13]
-      ubrowser = IncrementalBar('''installing browser''', max = len(bar))
+      ubrowser = IncrementalBar("installing os", max = len(bar))
       for item in bar:
          ubrowser.next()
          time.sleep(0.3)
       ubrowser.finish()
 
       kernel.main(1, 0, nameu)
-   elif ynn == '''n''':
+   elif ynn == "n":
       menu()
 
 def menu():
-   print('''\nwelcome to boot menu for diuz os version 0.2\n''')
-   print('''boot menu:''')
-   print('''[0] live''')
-   print('''[1] install''')
-   print('''[2] continue''')
-   print('''[3] exit''')
+   print("\nwelcome to boot menu for diuz os version 0.2\n")
+   print("boot menu:")
+   print("[0] live")
+   print("[1] install")
+   print("[2] continue")
+   print("[3] exit")
 
-   act = input('''[]: ''')
-   if act == '''0''':
-      kernel.main(1, 0, '''live''')
-   elif act == '''1''':
-      if os.path.isfile('''pass.pu''') == False or os.path.isfile('''name.nu''') == False:
+   act = input("[]: ")
+   if act == "0":
+      kernel.main(1, 0, "live")
+   elif act == "1":
+      if os.path.isfile("pass.pu") == False or os.path.isfile("name.nu") == False:
          install()
       else:
-         print('''\nyou have already installed the diuz os''')
+         print("\nyou have already installed the diuz os")
          menu()
-   elif act == '''2''':
-      if os.path.isfile('''pass.pu''') == False or os.path.isfile('''name.nu''') == False:
-         print('''\nyou cannot continue because you have not installed the diuz os''')
+   elif act == "2":
+      if os.path.isfile("pass.pu") == False or os.path.isfile("name.nu") == False:
+         print("\nyou cannot continue because you have not installed the diuz os")
          install()
       else:
-         f = open('''name.nu''')
+         f = open("name.nu")
          fd = f.read()
-         f1 = open('''pass.pu''')
+         f1 = open("pass.pu")
          fd1 = f1.read()
 
-         renameu = input('''\nrepeat your own name: ''')
-         repassu = input('''repeat your own pass: ''')
+         renameu = input("\nrepeat your own name: ")
+         repassu = input("repeat your own pass: ")
          text_to_bits(renameu)
          text_to_bits(repassu)
          while fd != text_to_bits(renameu) or fd1 != text_to_bits(repassu):
-            renameu = input('''\nrepeat your own name: ''')
-            repassu = input('''repeat your own pass: ''')
+            renameu = input("\nrepeat your own name: ")
+            repassu = input("repeat your own pass: ")
          kernel.main(1, 0, renameu)
    else:
       exit()
