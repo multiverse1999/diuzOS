4614fd2b6216186997fb46a23a7ac6cbf0f348b9
multiverse1999 <77222329+multiverse1999@users.noreply.github.com>
2025-02-24 17:02:20 +0000

    Update net.py

--- src/net.py
+++ src/net.py
@@ -15,9 +15,9 @@ print("your internet connection:")
 def test(mstr):
         res = requests.get(mstr)
         if res.status_code == 200:        
-            print(f"{'connected!!!'}")
+            print(f"connected!!!")
         else:
-            print(f"{'not connected...'}")
+            print(f"not connected...")
      
 if __name__ == "__main__":
     print(test(mstr = "http://www.ya.ru"))
