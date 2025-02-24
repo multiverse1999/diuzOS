6892d33f1c61a5ea18bc826909cbe610b8920398
multiverse1999 <77222329+multiverse1999@users.noreply.github.com>
2025-02-24 08:16:28 +0000

    Add files via upload

---
+++ src/browser.py
@@ -0,0 +1,28 @@
+# SPDX-License-Identifier: GPL-3.0 
+'''*
+   * diuz/version 0.2/browser.py
+   *
+   * Copyright (C) 2022 multiverse1999
+   *
+   * this file is browser
+   *'''
+import webbrowser
+
+print(''''link <line_url>' - open url in browser''')
+
+start = 1
+while start:
+	try:
+		search = input('''search: ''')
+		searchst = search.split()
+		if searchst[0] == '''link''':
+			ln = ''''''
+			for i in range(1, len(searchst)):
+				ln += searchst[i]
+				ln += ''' '''
+			webbrowser.open(ln)
+		elif searchst[0] == '''back''':
+			print('''\n''')
+			start = 0
+	except:
+	    pass 
